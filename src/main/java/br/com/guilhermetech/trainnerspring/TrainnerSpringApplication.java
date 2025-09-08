ee2238a65ff80cbb1fe6d9b0b296acdaa54f7ea3
Guilherme Hora <guilherme.andradehora@gmail.com>
2025-09-08 01:58:03 -0300

    Project created

---
+++ src/main/java/br/com/guilhermetech/trainnerspring/TrainnerSpringApplication.java
@@ -0,0 +1,13 @@
+package br.com.guilhermetech.trainnerspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainnerSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrainnerSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/guilhermetech/trainnerspring/TrainnerSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.guilhermetech.trainnerspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainnerSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
