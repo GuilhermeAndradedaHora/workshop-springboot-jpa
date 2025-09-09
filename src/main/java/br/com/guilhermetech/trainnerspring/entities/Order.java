07c26e5d1099112cea70453317006eaa18df93c8
Guilherme Hora <guilherme.andradehora@gmail.com>
2025-09-09 04:29:21 -0300

    OrderStatus enum

--- src/main/java/br/com/guilhermetech/trainnerspring/entities/Order.java
+++ src/main/java/br/com/guilhermetech/trainnerspring/entities/Order.java
@@ -1,8 +1,10 @@
 package br.com.guilhermetech.trainnerspring.entities;
 
+import br.com.guilhermetech.trainnerspring.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import jakarta.persistence.criteria.CriteriaBuilder;
 
 import java.io.Serializable;
 import java.time.Instant;
@@ -19,6 +21,8 @@ public class Order implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
+        private Integer orderStatus;
+
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
@@ -26,9 +30,10 @@ public class Order implements Serializable {
     public Order() {
     }
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -50,6 +55,16 @@ public class Order implements Serializable {
 
     public User getClient() {
         return client;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null){
+            this.orderStatus = orderStatus.getValue();
+        }
     }
 
     public void setClient(User client) {
